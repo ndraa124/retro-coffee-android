5ba160810f4163b6a580d57d634b5dd01691266a
AbdulDesu <49888665+AbdulDesu@users.noreply.github.com>
2021-01-22 15:11:41 +0700

    done welcome

--- app/src/main/java/com/id124/retrocoffee/activity/customer/onboard/OnboardActivity.kt
+++ app/src/main/java/com/id124/retrocoffee/activity/customer/onboard/OnboardActivity.kt
@@ -1,7 +1,9 @@
 package com.id124.retrocoffee.activity.customer.onboard
 
+import android.content.Intent
 import android.os.Bundle
 import com.id124.retrocoffee.R
+import com.id124.retrocoffee.activity.customer.welcome.WelcomeActivity
 import com.id124.retrocoffee.base.BaseActivity
 import com.id124.retrocoffee.databinding.ActivityOnboardBinding
 
@@ -9,5 +11,10 @@ class OnboardActivity : BaseActivity<ActivityOnboardBinding>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         setLayout = R.layout.activity_onboard
         super.onCreate(savedInstanceState)
+
+        bind.btGetStarted.setOnClickListener {
+            startActivity(Intent(this, WelcomeActivity::class.java))
+            finish()
+        }
     }
 }
--- app/src/main/java/com/id124/retrocoffee/activity/customer/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/id124/retrocoffee/activity/customer/welcome/WelcomeActivity.kt
@@ -1,7 +1,10 @@
 package com.id124.retrocoffee.activity.customer.welcome
 
+import android.content.Intent
 import android.os.Bundle
 import com.id124.retrocoffee.R
+import com.id124.retrocoffee.activity.customer.login.LoginActivity
+import com.id124.retrocoffee.activity.customer.register.RegisterActivity
 import com.id124.retrocoffee.base.BaseActivity
 import com.id124.retrocoffee.databinding.ActivityWelcomeBinding
 
@@ -9,5 +12,13 @@ class WelcomeActivity : BaseActivity<ActivityWelcomeBinding>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         setLayout = R.layout.activity_welcome
         super.onCreate(savedInstanceState)
+
+        bind.btCreateNewAccount.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
+        bind.btGoLogin.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 }
