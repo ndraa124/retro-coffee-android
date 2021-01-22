6a53b4c193dbc68f62f8138a84a1a87392f3ab4a
AbdulDesu <49888665+AbdulDesu@users.noreply.github.com>
2021-01-22 14:17:56 +0700

    fix layout

--- app/src/main/java/com/id124/retrocoffee/activity/customer/splash/SplashActivity.kt
+++ app/src/main/java/com/id124/retrocoffee/activity/customer/splash/SplashActivity.kt
@@ -54,6 +54,6 @@ class SplashActivity : BaseActivity<ActivitySplashBinding>(), SplashContract.Vie
     }
 
     override fun setError() {
-        Toast.makeText(this, "Login terlebih dahulu gan !", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "Please Login First !", Toast.LENGTH_SHORT).show()
     }
 }
