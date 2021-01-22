7ddbeaa7cc51d5988fdf5854413336b4a5bd4c65
AbdulDesu <49888665+AbdulDesu@users.noreply.github.com>
2021-01-22 19:03:50 +0700

    add favorite ui

---
+++ app/src/main/java/com/id124/retrocoffee/activity/customer/favorite/FavoriteContract.kt
@@ -0,0 +1,19 @@
+package com.id124.retrocoffee.activity.customer.favorite
+
+import com.id124.retrocoffee.model.favorite.FavoriteModel
+
+interface FavoriteContract {
+    interface View {
+        fun showFavoriteList(list: List<FavoriteModel>)
+        fun setError(error: String)
+        fun showProgressBar()
+        fun hideProgressBar()
+    }
+
+    interface Presenter{
+        fun bindToView(view: View)
+        fun unbind()
+        fun getFavorite()
+        fun deleteFavorite(favoriteID : String)
+    }
+}
