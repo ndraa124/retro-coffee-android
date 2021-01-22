87da1e15822018073ee3b644880f7ec33bfe0cfb
AbdulDesu <49888665+AbdulDesu@users.noreply.github.com>
2021-01-22 21:42:11 +0700

    fresh layout

---
+++ app/src/main/java/com/id124/retrocoffee/activity/customer/product_search/ProductSearchActivity.kt
@@ -0,0 +1,12 @@
+package com.id124.retrocoffee.activity.customer.product_search
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.id124.retrocoffee.R
+
+class ProductSearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_search)
+    }
+}
