642a7a5201f4fbdbdc6782671c6cc4b492ed9c9e
ndraa124 <indradavidpesik@gmail.com>
2021-01-24 22:18:49 +0800

    add feature product

---
+++ app/src/main/java/com/id124/retrocoffee/activity/customer/main/fragment/adapter/ProductAdapter.kt
@@ -0,0 +1,69 @@
+package com.id124.retrocoffee.activity.customer.main.fragment.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.id124.retrocoffee.R
+import com.id124.retrocoffee.databinding.ItemProductsBinding
+import com.id124.retrocoffee.model.product.ProductModel
+import com.id124.retrocoffee.remote.ApiClient.Companion.BASE_URL_IMAGE
+import com.id124.retrocoffee.util.Utils.Companion.currencyFormat
+
+class ProductAdapter : RecyclerView.Adapter<ProductAdapter.RecyclerViewHolder>() {
+    private lateinit var bind: ItemProductsBinding
+    private lateinit var onItemClickCallback: OnItemClickCallback
+    private var items = mutableListOf<ProductModel>()
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+
+    inner class RecyclerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(pr: ProductModel) {
+            bind.product = pr
+
+            if (pr.prPicImage != null) {
+                bind.imageUrl = BASE_URL_IMAGE + pr.prPicImage
+            }
+
+            bind.price = "IDR ${currencyFormat(pr.prPrice.toString())}"
+
+            bind.executePendingBindings()
+
+            itemView.setOnClickListener {
+                onItemClickCallback.onItemClick(pr)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewHolder {
+        bind = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.item_products,
+            parent,
+            false
+        )
+        return RecyclerViewHolder(bind.root)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerViewHolder, position: Int) {
+        holder.setIsRecyclable(false)
+        holder.bind(items[position])
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    interface OnItemClickCallback {
+        fun onItemClick(data: ProductModel)
+    }
+
+    fun addList(list: List<ProductModel>) {
+        items.clear()
+        items.addAll(list)
+        notifyDataSetChanged()
+    }
+}
--- app/src/main/java/com/id124/retrocoffee/service/ProductApiService.kt
+++ app/src/main/java/com/id124/retrocoffee/service/ProductApiService.kt
@@ -1,4 +1,16 @@
 package com.id124.retrocoffee.service
 
+import com.id124.retrocoffee.model.product.ProductResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
 interface ProductApiService {
+    @GET("product/category/{ctId}")
+    suspend fun getAllProductByCategory(
+        @Path("ctId") ctId: Int,
+        @Query("search") search: String? = null,
+        @Query("limit") limit: Int? = null,
+        @Query("page") page: Int? = null
+    ): ProductResponse
 }
