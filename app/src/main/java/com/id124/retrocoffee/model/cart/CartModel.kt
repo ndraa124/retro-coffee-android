e8471ff68ac354c17e5432dd3081a10872e4cb48
ndraa124 <indradavidpesik@gmail.com>
2021-01-25 22:50:57 +0800

    add feature favorite

--- app/src/main/java/com/id124/retrocoffee/model/cart/CartModel.kt
+++ app/src/main/java/com/id124/retrocoffee/model/cart/CartModel.kt
@@ -4,5 +4,26 @@ import com.google.gson.annotations.SerializedName
 
 data class CartModel(
     @SerializedName("cr_id")
-    val cr_id: Int
+    val crId: Int,
+
+    @SerializedName("cs_id")
+    val csId: Int,
+
+    @SerializedName("cr_product")
+    val crProduct: String,
+
+    @SerializedName("cr_price")
+    val crPrice: Long,
+
+    @SerializedName("cr_qty")
+    val crQty: Int,
+
+    @SerializedName("cr_total")
+    val crTotal: Long,
+
+    @SerializedName("cr_expired")
+    val crExpired: Int,
+
+    @SerializedName("cr_pic_image")
+    val crPicImage: String
 )
--- app/src/main/java/com/id124/retrocoffee/service/CartApiService.kt
+++ app/src/main/java/com/id124/retrocoffee/service/CartApiService.kt
@@ -1,4 +1,20 @@
 package com.id124.retrocoffee.service
 
+import com.id124.retrocoffee.model.cart.CartResponse
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
 interface CartApiService {
+    @FormUrlEncoded
+    @POST("cart")
+    suspend fun addCart(
+        @Field("cs_id") csId: Int,
+        @Field("cr_product") crProduct: String,
+        @Field("cr_price") crPrice: Long,
+        @Field("cr_qty") crQty: Int,
+        @Field("cr_total") crTotal: Long,
+        @Field("cr_expired") crExpired: Int,
+        @Field("cr_pic_image") crPicImage: String,
+    ): CartResponse
 }
