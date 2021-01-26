ab0eb19b35722731d268e947593b9ceb21feb2d5
AbdulDesu <49888665+AbdulDesu@users.noreply.github.com>
2021-01-26 18:16:15 +0700

    fix complete favorite

--- app/src/main/java/com/id124/retrocoffee/service/FavoriteApiService.kt
+++ app/src/main/java/com/id124/retrocoffee/service/FavoriteApiService.kt
@@ -1,11 +1,28 @@
 package com.id124.retrocoffee.service
 
 import com.id124.retrocoffee.model.favorite.FavoriteResponse
-import retrofit2.http.DELETE
-import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.*
 
 interface FavoriteApiService {
+
+    @FormUrlEncoded
+    @POST("favorite")
+    suspend fun addFavorite(
+        @Field("cs_id") csId: Int,
+        @Field("pr_id") prId: Int
+    ): FavoriteResponse
+
+    @GET("favorite/check")
+    suspend fun checkIsFavorite(
+        @Query("csId") csId: Int,
+        @Query("prId") prId: Int
+    ): FavoriteResponse
+
+    @DELETE("favorite")
+    suspend fun deleteFavoriteByProduct(
+        @Query("cs_id") csId: Int,
+        @Query("pr_id") prId: Int
+    ): FavoriteResponse
 
     @GET("favorite/{csId}")
     suspend fun getFavoriteProduct(@Path("csId") costumerID: Int) : FavoriteResponse
