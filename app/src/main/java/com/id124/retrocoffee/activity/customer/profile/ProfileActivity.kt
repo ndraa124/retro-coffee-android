560c26dd39ac9e2bcf228a1fbc462b258254d606
afarelramdani <ahmadfarhanel@gmail.com>
2021-01-27 09:06:24 +0700

    Add Feature history

--- app/src/main/java/com/id124/retrocoffee/activity/customer/profile/ProfileActivity.kt
+++ app/src/main/java/com/id124/retrocoffee/activity/customer/profile/ProfileActivity.kt
@@ -2,11 +2,13 @@ package com.id124.retrocoffee.activity.customer.profile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.id124.retrocoffee.R
+import com.id124.retrocoffee.activity.customer.history.HistoryActivity
 import com.id124.retrocoffee.base.BaseActivity
 import com.id124.retrocoffee.databinding.ActivityProfileBinding
 
-class ProfileActivity : BaseActivity<ActivityProfileBinding>() {
+class ProfileActivity : BaseActivity<ActivityProfileBinding>(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         setLayout = R.layout.activity_profile
         super.onCreate(savedInstanceState)
@@ -16,6 +18,14 @@ class ProfileActivity : BaseActivity<ActivityProfileBinding>() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         bind.toolbar.setNavigationOnClickListener {
             onBackPressed()
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.see_more -> {
+                intents<HistoryActivity>(this)
+            }
         }
     }
 }
--- app/src/main/java/com/id124/retrocoffee/service/HistoryApiService.kt
+++ app/src/main/java/com/id124/retrocoffee/service/HistoryApiService.kt
@@ -1,4 +1,13 @@
 package com.id124.retrocoffee.service
 
+import com.id124.retrocoffee.model.history.HistoryResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
 interface HistoryApiService {
+    @GET("history/{csId}")
+    suspend fun getAllHistoryOrder(
+        @Path("csId") csId: Int,
+    ): HistoryResponse
 }
